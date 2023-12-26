6d8a224306c1cc4d03da05becc7f98609aad262a
Satya2929 <121250321+Satya2929@users.noreply.github.com>
2023-12-26 12:06:13 +0530

    Add files via upload

---
+++ A3.java
@@ -0,0 +1,34 @@
+class A3 
+{
+	public static void main(String[] args) 
+	{
+		int a=5,b=10;
+		int  c;
+		c=a;
+		 a =b;
+		 b=c;
+		 System	.out.println("a"+a);
+		 System.out.println("b"+b);
+	}}
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
